602d62742b2366076ef16f9e588c65fb9dae4714
Chandan <91324501+chuphal@users.noreply.github.com>
2024-07-18 03:14:42 +0530

    added readme.MD and handle firestore csp error

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,11 @@ app.use(
       directives: {
         defaultSrc: ["'self'"],
         scriptSrc: ["'self'", "https://apis.google.com"],
-        connectSrc: ["'self'", "https://identitytoolkit.googleapis.com"],
+        connectSrc: [
+          "'self'",
+          "https://identitytoolkit.googleapis.com",
+          "https://firestore.googleapis.com",
+        ],
       },
     },
   })

599813b0c72d86b3e5a01eb86bfd3bc32b4fe7cf
Chandan <91324501+chuphal@users.noreply.github.com>
2024-07-17 21:50:10 +0530

    change file path

--- backend/data/items.js
+++ backend/data/items.js
@@ -1,14 +1,21 @@
 import fs from "node:fs/promises";
+import path from "path";
+const __dirname = path.resolve();
 
 async function getStoredItems() {
-  const rawFileContent = await fs.readFile("items.json", { encoding: "utf-8" });
+  const rawFileContent = await fs.readFile(__dirname + `/backend/items.json`, {
+    encoding: "utf-8",
+  });
   const data = JSON.parse(rawFileContent);
   const storedItems = data.items ?? [];
   return storedItems;
 }
 
 function storeItems(items) {
-  return fs.writeFile("items.json", JSON.stringify({ items: items || [] }));
+  return fs.writeFile(
+    __dirname + "/backend/items.json",
+    JSON.stringify({ items: items || [] })
+  );
 }
 
 export { getStoredItems, storeItems };
